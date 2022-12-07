88b9eff6aa7b55c74fe38fdf6811467cb1adbe9f
Lifeng Huang <nykfengcode@gmail.com>
2022-12-07 13:33:27 -0500

    view engine set to ejs

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 4080;
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const PORT = process.env.PORT || 4080;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("content/home");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
