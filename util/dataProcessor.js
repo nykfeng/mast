04db09d4e990162c4ef052e706e27731060f3d86
Lifeng Huang <nykfengcode@gmail.com>
2022-12-30 22:12:38 -0500

    refactor processDayToDateObject and processFullDateString functions

--- util/dataProcessor.js
+++ util/dataProcessor.js
@@ -7,6 +7,7 @@
 module.exports.newsDate = function (newsDateString) {
   // to store the date object converted from news piece data string
   let newsDate = null;
+  newsDateString = newsDateString.toLowerCase();
 
   // \b: This specifies a word boundary.
   // [0-9]{4}: This matches any four-digit number.
@@ -20,11 +21,11 @@ module.exports.newsDate = function (newsDateString) {
   if (!fourDigitYearRegex.test(newsDateString)) {
     //  1.1) test if the date string contains keywords like "today", "yesterday", "friday"
     if (dayRegex.test(newsDateString)) {
-      newsDate = processDayToDateObject(newsDateString.toLowerCase());
+      newsDate = processDayToDateObject(newsDateString);
       if (newsDate) return newsDate;
     }
 
-    //  1.2) test if the date string contains hours: minutes
+    //  1.2) test if the date string contains just hours:minutes like "16:45 ET"
     else if (hourColonMinuteRegex.test(newsDateString)) {
       newsDate = processHourMinuteTimeString(newsDateString);
       if (newsDate) return newsDate;
@@ -32,7 +33,8 @@ module.exports.newsDate = function (newsDateString) {
   }
   //  2) if it does contain full year, then we can try to interpret the date and convert to date object
   else {
-    
+    newsDate = processFullDateString(newsDateString);
+    if (newsDate) return newsDate;
   }
 
   return newsDate;
@@ -40,7 +42,6 @@ module.exports.newsDate = function (newsDateString) {
 
 function processDayToDateObject(dayStr) {
   let date = null;
-  let currentDay = date.getDay();
 
   // Example, if currentDay is 4 = Thursday, today's date is 12/29
   // a Monday would mean 3 days ago, so 1 - 4 = - 3
@@ -51,28 +52,22 @@ function processDayToDateObject(dayStr) {
   // date.setDate(25-6) = 12/19
   switch (true) {
     case dayStr.includes("mon"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(1, currentDay));
+      date = setDate(1); // 1 is monday, which is date.getDay()'s value
       break;
     case dayStr.includes("tue"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(2, currentDay));
+      date = setDate(2);
       break;
     case dayStr.includes("wed"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(3, currentDay));
+      date = setDate(3);
       break;
     case dayStr.includes("thu"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(4, currentDay));
+      date = setDate(4);
       break;
     case dayStr.includes("fri"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(5, currentDay));
+      date = setDate(5);
       break;
     case dayStr.includes("sat"):
-      date = new Date();
-      date.setDate(date.getDate() + dayDifferences(6, currentDay));
+      date = setDate(6);
       break;
     case dayStr.includes("sun"):
       date = new Date();
@@ -89,6 +84,16 @@ function processDayToDateObject(dayStr) {
     // If the input keyword is not recognized, throw an error
     // throw new Error(`Invalid input: ${dayStr}`);
   }
+  return date;
+}
+
+function setDate(weekdayNumber) {
+  const date = new Date();
+  const currentDay = date.getDay();
+
+  // set the date object to be the date of the input day, like the past "monday" date
+  date.setDate(date.getDate() + dayDifferences(weekdayNumber, currentDay));
+
   return date;
 }
 
@@ -115,6 +120,22 @@ function processHourMinuteTimeString(timeStr) {
   const date = new Date(`${year}-${month + 1}-${day}T${sanitizedTimeStr}`);
 
   return date;
+}
+
+function processFullDateString(dateStr) {
+  // need to sanitize time zone for the Date object to work
+  // Example date string inputs
+  // 1) 'Dec 27, 2022, 09:15 ET'
+  // 2) '2022-12-22T12:49:00Z'
+  // 3) 'December 29, 2022 17:00 ET'
+
+  // Remove the letters at the end and after the last space
+  const sanitizedDateStr = dateStr.replace(/\s*[a-zA-Z]*$/, "").trim();
+  // The regular expression /\s*[a-zA-Z]*$/ will match zero or more spaces followed by zero or more letters at the end of the string.
+  // the 's' character is used to match any white space character, including the space, tab, and newline characters.
+  // the '*' character is a quantifier that indicates that the preceding character or group of characters should be matched zero or more times.
+
+  return new Date(sanitizedDateStr);
 }
 
 // ===== news title string formatter
